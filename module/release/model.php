abad2ea62f7306e7754a31e225e3b920dda4b686
tianshujie <tianshujie@easycorp.ltd>
2026-08-05 16:05:27 +0800

    * [task#164249,done,1h] Remove action exclude actiavte, delete.

--- module/release/model.php
+++ module/release/model.php
@@ -877,6 +877,7 @@ class releaseModel extends model
 
         $action = strtolower($action);
 
+        if($release->status == 'terminate' && !in_array($action, array('play', 'delete'))) return false;
         if($action == 'notify')  return ($release->bugs || $release->stories) && $release->status == 'normal';
         if($action == 'play')    return $release->status == 'terminate';
         if($action == 'pause')   return $release->status == 'normal';
