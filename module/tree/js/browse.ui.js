7490b7759742da423fe1edc98ed13181aa2fd0c7
wangyidong <yidong@cnezsoft.com>
2026-08-07 08:59:43 +0800

    * [refac bug #74687] Adjust browse task module.

--- module/tree/js/browse.ui.js
+++ module/tree/js/browse.ui.js
@@ -66,36 +66,6 @@ function syncProduct(obj)
     });
 }
 
-window.addItem = function(e)
-{
-    const obj     = e.target
-    const thisRow = $(obj).closest('.form-row');
-    const newItem = thisRow.clone();
-
-    newItem.find('.add-btn').on('click', addItem);
-    newItem.find('.del-btn').on('click', removeItem);
-    $newBranch = newItem.find('.picker-box [name^=branch]');
-    $pickerBox = null;
-    if($newBranch.length > 0)
-    {
-        $pickerBox = $newBranch.closest('.picker-box');
-        $pickerBox.removeAttr('id').removeAttr('data-zui-picker').empty();
-    }
-
-    $(obj).closest('.form-row').after(newItem);
-
-    newItem.find('input[id^=modules]').attr('id', 'modules[]').attr('name', 'modules[]').val('');
-    newItem.find('input[id^=shorts]').attr('id', 'shorts[]').attr('name', 'shorts[]').val('');
-    newItem.find('input[id^=order]').attr('id', 'order[]').attr('name', 'order[]');
-    newItem.find('.existing-actions.action-group').removeClass('existing-actions');
-
-    if($pickerBox)
-    {
-        options = thisRow.find('.picker-box [name^=branch]').zui('picker').options;
-        $pickerBox.picker($.extend({'name': 'branch[]'}, options));
-    }
-}
-
 window.toggleCopy = function()
 {
    $('.form-row.copyBox').toggleClass('hidden');
