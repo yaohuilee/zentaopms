efbc190cfc74b3aba9f0ce17b217618c30ddd212
liyang <liyang@easycorp.ltd>
2026-08-11 08:47:11 +0800

    * [task#164525] add batch AssignTo in review list.

--- module/repo/ui/review.html.php
+++ module/repo/ui/review.html.php
@@ -49,6 +49,17 @@ foreach($bugs as $bug)
         $bug->link = $this->repo->createLink('diff', "repoID={$bug->repo}&objectID={$objectID}&entry={$fileEntry}&oldRevision={$v1}&newRevision={$v2}");
     }
 }
+if(hasPriv('bug', 'batchAssignTo'))
+{
+    $pinyinItems     = common::convert2Pinyin($users);
+    $assignedToItems = array();
+    foreach($users as $key => $value)
+    {
+        $key = base64_encode((string)$key); // 编码用户名中的特殊字符
+        $assignedToItems[] = array('text' => $value, 'keys' => zget($pinyinItems, $value, ''), 'innerClass' => 'batch-btn ajax-btn not-open-url', 'data-url' => helper::createLink('bug', 'batchAssignTo', "assignedTo=$key&productID=0&type=product"));
+    }
+    $footToolbar['items'][] = array('caret' => 'up', 'text' => $lang->bug->assignedTo, 'type' => 'dropdown', 'data-placement' => 'top-start', 'items' => $assignedToItems, 'data-menu' => array('searchBox' => true));
+}
 $bugs = initTableData($bugs, $config->repo->reviewDtable->fieldList);
 
 if($app->tab == 'project' || $app->tab == 'execution') $repoID = 0;
@@ -66,5 +77,7 @@ dtable
     set::sortLink(createLink('repo', 'review', "repoID=$repoID&browseType=$browseType&objectID={$objectID}&orderBy={name}_{sortType}&recTotal={$pager->recTotal}&recPerPage={$pager->recPerPage}&pageID={$pager->pageID}")),
     set::orderBy($orderBy),
     set::onRenderCell(jsRaw('window.renderRepobugList')),
+    hasPriv('bug', 'batchAssignTo') ? set::footToolbar($footToolbar) : null,
+    set::onCheckChange(jsRaw('window.checkedChange')),
     set::footPager(usePager())
 );
