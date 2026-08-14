43c3072b464a015a30ba4d0c6fd66b371a9a38a4
tianshujie <tianshujie@easycorp.ltd>
2026-08-14 16:58:06 +0800

    * [bug#74928,done,0.1h] Set stagedBy is realname.

--- module/action/config.php
+++ module/action/config.php
@@ -136,7 +136,7 @@ $config->trash->search['params']['date']       = array('operator' => '=', 'contr
 $config->action->newPageModule  = array('repo', 'ppm', 'host', 'account', 'serverroom', 'instance', 'store', 'space', 'domain', 'service', 'gitlab', 'gitea', 'gogs', 'sonarqube', 'jenkins', 'nexus', 'board');
 $config->action->latestDateList = array('today', 'yesterday', 'thisWeek', 'lastWeek', 'thisMonth');
 
-$config->action->userFields         = 'openedBy,addedBy,createdBy,editedBy,assignedTo,finishedBy,canceledBy,closedBy,activatedBy,resolvedBy,lastEditedBy,builder,owner,reviewedBy,forwardBy,scriptedBy,manager,commitedBy,archivedBy,PO,QD,RD,feedback,PM,account,changedBy,submitedBy,retractedBy,lastRunner,assignedBy,processedBy';
+$config->action->userFields         = 'openedBy,addedBy,createdBy,editedBy,assignedTo,finishedBy,canceledBy,closedBy,activatedBy,resolvedBy,lastEditedBy,builder,owner,reviewedBy,forwardBy,scriptedBy,manager,commitedBy,archivedBy,PO,QD,RD,feedback,PM,account,changedBy,submitedBy,retractedBy,lastRunner,assignedBy,processedBy,stagedBy';
 $config->action->multipleUserFields = 'mailto,whitelist,reviewer,users,assignee,approver,PMT,committer,backReviewers,contributor,reviewers,editedList';
 $config->action->showNameFields     = array('resolvedBuild' => 'build');
 
