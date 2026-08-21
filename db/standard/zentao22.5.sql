a1b2ff48712fab81a2a2dfd4f9b85c310b5ad277
liugang <liugang@chandao.com>
2026-08-21 14:02:33 +0800

    * [feedback#11697] adjust standard sqls.

--- db/standard/zentao22.5.sql
+++ db/standard/zentao22.5.sql
@@ -1985,6 +1985,7 @@ CREATE TABLE `zt_cron` (
   `buildin` tinyint unsigned NOT NULL DEFAULT '0',
   `status` varchar(20) NOT NULL DEFAULT '',
   `lastTime` datetime DEFAULT NULL,
+  `timeout` smallint unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   KEY `lastTime` (`lastTime`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -3808,12 +3809,15 @@ CREATE TABLE `zt_queue` (
   `type` varchar(255) NOT NULL DEFAULT '',
   `command` text DEFAULT NULL,
   `status` varchar(10) NOT NULL DEFAULT 'wait',
+  `pending` tinyint unsigned NULL DEFAULT NULL,
   `execId` int unsigned NOT NULL DEFAULT '0',
   `createdDate` datetime DEFAULT NULL,
+  `startedDate` datetime DEFAULT NULL,
   `deleted` tinyint unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `status_createdDate` (`status`,`createdDate`),
-  KEY `cron_createdDate` (`cron`,`createdDate`)
+  KEY `cron_createdDate` (`cron`,`createdDate`),
+  UNIQUE KEY `uk_cron_pending` ON `zt_queue`(`cron`, `pending`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 CREATE TABLE `zt_relation` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
