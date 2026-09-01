3cccf8a8c0bfbeb31921fb874c7b3aca042835db
liyang <liyang@easycorp.ltd>
2026-09-01 08:55:47 +0800

    * [task#165787] fix mr lang.

--- module/ppm/lang/zh-cn.php
+++ module/ppm/lang/zh-cn.php
@@ -287,7 +287,7 @@ $lang->ppm->notDelbranch      = "源分支为受保护分支时不可删除";
 $lang->ppm->addForApp         = "该服务器下没有项目，是否前往添加？";
 $lang->ppm->checkSuccess      = '检查已通过，此分支允许合并';
 $lang->ppm->checkFailed       = '检查未通过，此分支无法合并';
-$lang->ppm->MRHistory         = "本次合并由 <strong>%s</strong> 于 <strong>%s</strong> 创建，申请将 <label class='label primary size-sm px-2 cursor-pointer' data-on='click' data-call='copy' data-params='event'>%s<icon class='icon-copy ml-1'/></label> 的 <strong>%s</strong> 次提交，合并到 <label class='label primary size-sm px-2 cursor-pointer' data-on='click' data-call='copy' data-params='event'>%s<icon class='icon-copy ml-1'/></label> 。";
+$lang->ppm->MRHistory         = "本次合并由 <strong>%s</strong> 于 <strong>%s</strong> 创建，申请将 <icon class='icon-code-fork mr-1'/><strong>%s</strong> 的 <strong>%s</strong> 次提交，合并到 <icon class='icon-code-fork mr-1'><strong>%s</strong> 。";
 
 $lang->ppm->checkStatusList = array();
 $lang->ppm->checkStatusList['fail']    = '未通过';
