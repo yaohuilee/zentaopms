b3a3817d878f623c917bd9c9b0ff16f566619400
liugang <liugang@chandao.com>
2026-09-02 13:08:52 +0800

    + [errorlog] set fixed column widths and truncate long messages in error log list

--- module/errorlog/config/dtable.php
+++ module/errorlog/config/dtable.php
@@ -30,36 +30,49 @@ $config->errorlog->dtable->fieldList['requestID']['name']     = 'requestID';
 $config->errorlog->dtable->fieldList['requestID']['title']    = $lang->errorlog->requestID;
 $config->errorlog->dtable->fieldList['requestID']['type']     = 'text';
 $config->errorlog->dtable->fieldList['requestID']['sortType'] = true;
+$config->errorlog->dtable->fieldList['requestID']['width']    = '190';
+$config->errorlog->dtable->fieldList['requestID']['flex']     = false;
 
 $config->errorlog->dtable->fieldList['module']['name']     = 'module';
 $config->errorlog->dtable->fieldList['module']['title']    = $lang->errorlog->module;
 $config->errorlog->dtable->fieldList['module']['type']     = 'text';
 $config->errorlog->dtable->fieldList['module']['sortType'] = true;
+$config->errorlog->dtable->fieldList['module']['width']    = '90';
+$config->errorlog->dtable->fieldList['module']['flex']     = false;
 
 $config->errorlog->dtable->fieldList['method']['name']     = 'method';
 $config->errorlog->dtable->fieldList['method']['title']    = $lang->errorlog->method;
 $config->errorlog->dtable->fieldList['method']['type']     = 'text';
 $config->errorlog->dtable->fieldList['method']['sortType'] = true;
+$config->errorlog->dtable->fieldList['method']['width']    = '130';
+$config->errorlog->dtable->fieldList['method']['flex']     = false;
 
 $config->errorlog->dtable->fieldList['level']['name']     = 'level';
 $config->errorlog->dtable->fieldList['level']['title']    = $lang->errorlog->level;
 $config->errorlog->dtable->fieldList['level']['type']     = 'text';
 $config->errorlog->dtable->fieldList['level']['sortType'] = true;
+$config->errorlog->dtable->fieldList['level']['width']    = '110';
+$config->errorlog->dtable->fieldList['level']['flex']     = false;
 
 $config->errorlog->dtable->fieldList['account']['name']     = 'account';
 $config->errorlog->dtable->fieldList['account']['title']    = $lang->errorlog->account;
 $config->errorlog->dtable->fieldList['account']['type']     = 'text';
 $config->errorlog->dtable->fieldList['account']['sortType'] = true;
+$config->errorlog->dtable->fieldList['account']['width']    = '90';
+$config->errorlog->dtable->fieldList['account']['flex']     = false;
 
 $config->errorlog->dtable->fieldList['createdDate']['name']     = 'createdDate';
 $config->errorlog->dtable->fieldList['createdDate']['title']    = $lang->errorlog->createdDate;
 $config->errorlog->dtable->fieldList['createdDate']['type']     = 'datetime';
 $config->errorlog->dtable->fieldList['createdDate']['sortType'] = true;
+$config->errorlog->dtable->fieldList['createdDate']['width']    = '160';
 
 $config->errorlog->dtable->fieldList['message']['name']     = 'message';
 $config->errorlog->dtable->fieldList['message']['title']    = $lang->errorlog->message;
 $config->errorlog->dtable->fieldList['message']['type']     = 'text';
 $config->errorlog->dtable->fieldList['message']['flex']     = true;
+$config->errorlog->dtable->fieldList['message']['minWidth'] = '200';
+$config->errorlog->dtable->fieldList['message']['cellStyle'] = array('whiteSpace' => 'nowrap', 'overflow' => 'hidden', 'textOverflow' => 'ellipsis');
 
 $config->errorlog->dtable->fieldList['actions']['title'] = $lang->actions;
 $config->errorlog->dtable->fieldList['actions']['type']  = 'actions';
