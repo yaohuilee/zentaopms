39bde35176b3558b4ff0bd5d8d750425345e99b8
sunguangming <sunguangming@chandao.com>
2026-09-02 10:59:58 +0800

    * [bug#75321,done,0.2h] fix bug.

--- module/caselib/zen.php
+++ module/caselib/zen.php
@@ -71,7 +71,7 @@ class caselibZen extends caselib
         $this->config->testcase->search['params']['module']['values'] = $this->loadModel('tree')->getOptionMenu($libID, 'caselib');
 
         /* Unset fields for search. */
-        if(!$this->config->testcase->needReview) unset($this->config->testcase->search['params']['status']['values']['wait']);
+        if(!$this->config->testcase->needReview && empty($this->config->testcase->forceReview)) unset($this->config->testcase->search['params']['status']['values']['wait']);
         unset($this->config->testcase->search['fields']['product']);
         unset($this->config->testcase->search['params']['product']);
         unset($this->config->testcase->search['fields']['branch']);
