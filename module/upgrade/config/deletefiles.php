29845f08aa4e0d5ef119434e6408e49fbdc4226d
wangyuting <wangyuting@chandao.com>
2026-09-04 07:01:36 +0000

    * Delete testcase search config file.

--- module/upgrade/config/deletefiles.php
+++ module/upgrade/config/deletefiles.php
@@ -2640,3 +2640,4 @@ $config->delete['22_3'][] = 'module/svn';
 $config->delete['22_3'][] = 'module/compile';
 $config->delete['22_5'][] = 'extension/custom/runner';
 $config->delete['22_5'][] = 'module/upgrade/css/execute.ui.css';
+$config->delete['22_6'][] = 'module/testcase/config/search.php';
