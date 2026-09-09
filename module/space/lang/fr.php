2ba3202fe7c4858c04a47ba187fa65b92252df76
liyang <liyang@easycorp.ltd>
2026-09-09 16:54:00 +0800

    * [task#166334] add code check.

--- module/space/lang/fr.php
+++ module/space/lang/fr.php
@@ -71,6 +71,7 @@ $lang->space->notice->accessArtifact        = 'Only display users who can access
 $lang->space->notice->confirmRemoveMember   = 'Are you sure to remove this user from this space?';
 $lang->space->notice->confirmDelete         = 'Are you sure delete %s permission group?';
 $lang->space->notice->managerMemberConflict = '%s is a space user. To configure them as an administrator, you can remove the user first.';
+$lang->space->notice->codeNotSupportUppercase = 'Code cannot contain uppercase letters';
 
 $lang->space->placeholder = new stdclass();
 $lang->space->placeholder->desc = 'Description of this space';
--- module/space/lang/zh-cn.php
+++ module/space/lang/zh-cn.php
@@ -71,6 +71,7 @@ $lang->space->notice->accessArtifact        = '仅展示用户可访问的非公
 $lang->space->notice->confirmRemoveMember   = '您确定从该空间中移除该用户吗？';
 $lang->space->notice->confirmDelete         = '您确定删除“ %s” 权限分组吗？';
 $lang->space->notice->managerMemberConflict = '%s为空间用户，如需配置为管理员可先移除用户。';
+$lang->space->notice->codeNotSupportUppercase = '唯一标识不能包含大写字母';
 
 $lang->space->placeholder       = new stdclass();
 $lang->space->placeholder->desc = '请输入空间描述';
