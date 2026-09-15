d54d4fb9761feadd43d40ad6aa33c24bd3ad3b9c
wangyuting <wangyuting@chandao.com>
2026-09-15 05:53:37 +0000

    * Optimize code.

--- www/index.php
+++ www/index.php
@@ -59,7 +59,8 @@ $handler = static function ()
         $common->checkPriv();
         $common->checkIframe();
 
-        $isClient = !empty($_SERVER['HTTP_USER_AGENT']) && (strpos($_SERVER['HTTP_USER_AGENT'], 'xuanxuan') !== false || strpos($_SERVER['HTTP_USER_AGENT'], 'uni-app') !== false);
+        $userAgent = $_SERVER['HTTP_USER_AGENT'] ?? '';
+        $isClient  = strpos($userAgent, 'xuanxuan') !== false || strpos($userAgent, 'uni-app') !== false;
         if($app->getViewType() != 'json' && session_id() != $app->sessionID && !$isClient) helper::restartSession($app->sessionID);
 
         /* Remove install.php and upgrade.php. */
