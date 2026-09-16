ce1e779823493e25a2418a7ee7d51e85c802523c
lihuifang <lihuifang@easycorp.ltd>
2026-09-16 13:40:28 +0800

    *[uitest] Modify element.

--- module/projectrelease/test/ui/page/view.php
+++ module/projectrelease/test/ui/page/view.php
@@ -5,7 +5,7 @@ class viewPage extends page
     {
         parent::__construct($webdriver);
         $xpath = array(
-            'basic'               => "//*[@id='mainContent']/div[2]/div/div/div/div/ul/li[4]/a/span",
+            'basic'               => "//*[@id='releaseTabs']/div[1]/ul/li[5]",
             'basicSystemName'     => "//*[@id='releaseInfo']/div[2]/div[1]/table/tbody/tr[2]/td",
             'basicreleasename'    => "//*[@id='releaseInfo']/div[2]/div[1]/table/tbody/tr[3]/td",
             'basicstatus'         => "//*[@id='releaseInfo']/div[2]/div[1]/table/tbody/tr[4]/td",
