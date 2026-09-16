f1bb7c39eb2cc44b153b703dff1a88117d310779
daitingting <daitingting@xirangit.com>
2026-09-16 16:26:34 +0800

    * [bug#75722,done,0.1h] Fix bug #75722.

--- module/execution/ui/view.html.php
+++ module/execution/ui/view.html.php
@@ -315,7 +315,7 @@ div
             ),
             div
             (
-                set::className('detail-content mt-4 overflow-hidden desc-box'),
+                set::className('detail-content mt-4 overflow-hidden desc-box overflow-y-scroll scrollbar-thin scrollbar-hover'),
                 set::title(strip_tags($execution->desc)),
                 html($execution->desc)
             )
