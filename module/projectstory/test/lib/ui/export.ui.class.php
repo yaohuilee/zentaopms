9460a475eda6ec81b33551068d310ec57747301c
lihuifang <lihuifang@easycorp.ltd>
2026-09-16 14:47:35 +0800

    *[uitest] Modify data.

--- module/projectstory/test/lib/ui/export.ui.class.php
+++ module/projectstory/test/lib/ui/export.ui.class.php
@@ -14,9 +14,9 @@ class exportTester extends tester
         $form = $this->initForm('projectstory', 'story', array('project' => '1'), 'appIframe-project');
         $form->dom->exportBtn->click();
         if(isset($projectStory['filename'])) $form->dom->fileName->setValue($projectStory['filename']);
-        if(isset($projectStory['format']))   $form->dom->format->picker($projectStory['format']);
-        if(isset($projectStory['encoding'])) $form->dom->encoding->picker($projectStory['encoding']);
-        if(isset($projectStory['data']))     $form->dom->data->picker($projectStory['data']);
+        if(isset($projectStory['format']))   $form->dom->fileType->picker($projectStory['format']);
+        if(isset($projectStory['encoding'])) $form->dom->encode->picker($projectStory['encoding']);
+        if(isset($projectStory['data']))     $form->dom->exportType->picker($projectStory['data']);
         $form->dom->exportBtnAlert->click();
         $form->wait(2);
 
