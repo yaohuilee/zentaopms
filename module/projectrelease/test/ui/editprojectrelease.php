6c144dee680c49a265fca39cd9b7683fa48be168
lihuifang <lihuifang@easycorp.ltd>
2026-09-16 14:36:15 +0800

    *[uitest] Modify code.

--- module/projectrelease/test/ui/editprojectrelease.php
+++ module/projectrelease/test/ui/editprojectrelease.php
@@ -90,15 +90,11 @@ $release = array(
     array('name' => 'release1'),
     array('systemname' => '应用BBB'),
     array('name' => 'edit-release-wait' . time(), 'status' => '未开始', 'plandate' => date('Y-m-d', strtotime('+5 day'))),
-    array('name' => 'edit-release-normal' . time(), 'status' => '已发布', 'plandate' => date('Y-m-d', strtotime('+10 day')), 'releasedate' => date('Y-m-d', strtotime('+1 month'))),
-    array('name' => 'edit-release-terminate' . time(), 'status' => '停止维护', 'plandate' => date('Y-m-d', strtotime('+1 month')), 'releasedate' => date('Y-m-d', strtotime('+5 days'))),
 );
 
 r($tester->editRelease($release['0'])) && p('message,status') && e('编辑项目发布表单页必填提示信息正确,SUCCESS'); // 发布名称置空保存，检查提示信息
 r($tester->editRelease($release['1'])) && p('message,status') && e('发布名称重复时提示信息正确,SUCCESS');         // 发布名称重复时提示信息正确
 r($tester->editRelease($release['2'])) && p('message,status') && e('编辑项目发布成功,SUCCESS');                   // 编辑发布，修改应用
 r($tester->editRelease($release['3'])) && p('message,status') && e('编辑项目发布成功,SUCCESS');                   // 编辑发布，修改名称、状态改为已发布、计划日期、发布日期
-r($tester->editRelease($release['4'])) && p('message,status') && e('编辑项目发布成功,SUCCESS');                   // 编辑发布，修改名称、状态改为停止维护
-r($tester->editRelease($release['5'])) && p('message,status') && e('编辑项目发布成功,SUCCESS');                   // 编辑发布，修改名称、状态改为停止维护
 
 $tester->closeBrowser();
